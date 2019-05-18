bb9cb946530dbe881377e28b66f71292bafe99bc
Hichem.K <47053723+Koched92@users.noreply.github.com>
2019-05-18 14:05:54 +0200

    Update Jena_code_SPARQL.java

--- Scripts/Jena_code_SPARQL.java
+++ Scripts/Jena_code_SPARQL.java
@@ -1,15 +1,13 @@
-public class EssaiModèle {
+public class ExtractEntities {
 
 	private static final String WHERE = null;
 
 	public static void query_model(String arg) throws IOException, ParseException {
 		
 		Model model = FileManager.get().loadModel(arg);
-		//model.write(System.out,"Turtle");// (RDF/XML) & (N-triples) & (N3) & (Turtle)
-		//essai d'application d'une requête qui extrait les noms des artistes à partir du fichier "5325.tll"
 		String queryString = 
 				
-
+			/////////////////////////Adding prefixes/////////////////////////
 "prefix schema: <http://schema.org/>"+
 "prefix cro:   <http://rhizomik.net/ontologies/copyrightonto#>"+
 "prefix owl:   <http://www.w3.org/2002/07/owl#>"+
@@ -23,59 +21,25 @@ public class EssaiModèle {
 "prefix prov:  <http://www.w3.org/ns/prov#> "+
 "prefix foaf:  <http://xmlns.com/foaf/0.1/> "+
 "prefix dc:    <http://purl.org/dc/elements/1.1/> "+
-
-	/////////////////////////Requête de la base F31//////////////////
-//"SELECT    ?t1   ?d2  "+
-//"WHERE{"+
-//
-//"?eventPrevu a mus:M26_Foreseen_Performance;"+
-//"ecrm:P102_has_title ?t1_uri."+
-//"?t1_uri rdfs:label ?t1."+
-//"?eventPrevu a mus:M26_Foreseen_Performance;"+
-//"mus:U8_foresees_time_span ?d2_uri."+
-//"?d2_uri rdfs:label ?d2."+
-
-
-"SELECT  ?t ?t2 ?d ?d2 ?e "+
-"WHERE{"+ 
-
-
-
-"?eventPrevu a mus:M26_Foreseen_Performance;"+
-"mus:U8_foresees_time_span ?d_uri."+
-"?d_uri rdfs:label ?d."+
-
-"?eventPrevu a mus:M26_Foreseen_Performance;"+
-"ecrm:P102_has_title ?t."+
-
-"?event a efrbroo:F29_Recording_Event;"+
-"ecrm:P32_used_general_technique ?e;"+
-"ecrm:P4_has_time-span ?d2_uri."+
-"?d2_uri rdfs:label ?d2."+
-"?event a efrbroo:F29_Recording_Event;"+
-"efrbroo:R20_recorded ?t2_uri."+
-"?t2_uri ecrm:P102_has_title ?t2."+
-"}";
-	/////////////////////////Requête de la base M26//////////////////
-//		          "SELECT ?id ?d ?p ?t "+
-//				  "WHERE{"+ 
-//				  "?eventPrevu a mus:M26_Foreseen_Performance;"+
-//				  "mus:U7_foresees_place_at ?p_uri."+
-//				  
-//				  "?p_uri rdfs:label ?p."+
-//				  "?eventPrevu a mus:M26_Foreseen_Performance;"+
-//
-//				  "mus:U8_foresees_time_span ?d_uri."+
-//				  "?d_uri rdfs:label ?d."+
-//				  "?eventPrevu a mus:M26_Foreseen_Performance;"+
-//
-//				  "ecrm:P102_has_title ?t."+
-//				  "?eventPrevu a mus:M26_Foreseen_Performance;"+
-//
-//				  "dc:identifier ?id."+
-//				  "?eventPrevu a mus:M26_Foreseen_Performance;"+
-//
-//				  "}ORDER BY ?id";
+			/////////////////////////SPARQL Request for F31 class/////////////////////////
+			
+	"SELECT  ?uri ?title ?date"+
+		"WHERE{"+ 
+			"?eventPrevu a efrbroo:F29_Recording_Event;"+
+			"ecrm:P4_has_time-span  ?date_uri."+
+			"?date_uri rdfs:label ?date."+
+			"?event a efrbroo:F31_Performance;" + 
+			"ecrm:P102_has_title ?title."+
+		"}";
+			/////////////////////////SPARQL Request for M26 class/////////////////////////
+//	"SELECT ?uri ?title ?date"+
+//		 "WHERE{"+ 
+//			"?eventPrevu a mus:M26_Foreseen_Performance;"+
+//		        "mus:U8_foresees_time_span ?date_uri."+
+//			"?date_uri rdfs:label ?date."+
+//			"?eventPrevu a mus:M26_Foreseen_Performance;"+
+//		        "ecrm:P102_has_title ?title."+
+//	        "}";
 			
 		Query query = QueryFactory.create(queryString);
 		QueryExecution qexec = QueryExecutionFactory.create(query, model);
@@ -84,23 +48,13 @@ public class EssaiModèle {
 			
 			while (results.hasNext()) {
 				QuerySolution soln = results.nextSolution();
-//				Literal p = soln.getLiteral("p");
-//			    Literal n = soln.getLiteral("n");
-//				Literal t = soln.getLiteral("t");
-//				
-//				System.out.println(t+ "----"+n+"---");
-				ResultSetFormatter.out(System.out,results,query);
-				//simple way to print result
-				//model.write(System.out,"Turtle");
-			}
-			
-		}finally {
-			
-			
-		}
-		
-		
-	}
-
-		
+					Literal uri = soln.getLiteral("uri");
+				        Literal title = soln.getLiteral("title");
+					Literal date = soln.getLiteral("date");
+				
+			System.out.println(uri+","+title+","+date);
+			}		
+		}finally {		
+		}	
+	}	
 	}
